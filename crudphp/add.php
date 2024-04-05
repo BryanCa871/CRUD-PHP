f7899a47d7db2bcdf8042f685244af8c56bc891b
BryanCa871 <bryansoe871@gmail.com>
2024-04-05 17:21:01 -0600

    Update add.php

--- crudphp/add.php
+++ crudphp/add.php
@@ -6,7 +6,7 @@ if(!empty($_POST['producto'])){
     $producto = $_POST['producto'];
     $categoria = $_POST['categoria'];
     $query = mysqli_query($coon, "INSERT INTO productos(producto,categoria) VALUES('$producto', '$categoria')");
-
+//
     if($query){
         header("Location: index.php");
     }
